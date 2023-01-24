ccd3951460e6561e0b9d40c8928c9b096e0a7f5e
mohsin.khan <mohsin.khan@niveussolutions.com>
2023-01-24 13:06:30 +0530

    new changes

--- src/main/java/com/niveus/springcicddemo/controllers/TestController.java
+++ src/main/java/com/niveus/springcicddemo/controllers/TestController.java
@@ -6,15 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+	
 
 	@Autowired
 	private MessageService messageService;
-
+	
+	@Autowired
+    RestTemplate restTemplate;
+	
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam String user) {
 		return messageService.getSubscriptionMessage(user);
@@ -34,5 +39,18 @@ public class TestController {
 	public String getVersion3() {
 		return "ISTIO - Version 3.0";
 	}
+	
+	@GetMapping("/greeting")
+	public String doGreeting(@RequestParam String user) {
+		
+		if(user.equalsIgnoreCase("mohsin")){ 
+			return restTemplate.getForObject("http://springgreeting.default.svc.cluster.local:9090/api/greeting/hello?user=Mohsin", String.class);
+		}
+		else {
+			return restTemplate.getForObject("http://springgreeting.default.svc.cluster.local:9090/api/greeting/exception/arithmetic", String.class);
+		}
+		
+	}
+	
 
 }
